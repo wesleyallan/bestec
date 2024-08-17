6863fbb1950017b532f2c7bcbc678f54b50aa549
Wesley Silva <wesley.allansilva@gmail.com>
2024-08-17 19:44:48 -0300

    chore: Convert to use Tailwindcss and adjust the pages

--- bestec.py
+++ bestec.py
@@ -8,47 +8,6 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-# Login e Registro
 @app.route('/login')
 def login():
   return render_template("login.html")
-
-@app.route('/registration/categories')
-def registration_categories():
-  return render_template("registration_categories.html")
-
-@app.route('/reports')
-def reports():
-  return render_template("reports.html")
-
-@app.route('/reports/sales')
-def reports_sales():
-  return render_template("reports_sales.html")
-
-@app.route('/reports/ads')
-def reports_ads():
-  return render_template("reports_ads.html")
-
-@app.route('/user')
-def user():
-  return render_template("user.html")
-
-@app.route('/user/data')
-def user_data():
-  return render_template("user_data.html")
-
-@app.route('/user/ads')
-def user_ads():
-  return render_template("user_ads.html")
-
-@app.route('/user/ads/new')
-def user_ads_new():
-  return render_template("user_ads_new.html")
-
-@app.route('/user/requests')
-def user_requests():
-  return render_template("user_requests.html")
-
-@app.route('/user/favorites')
-def user_favorites():
-  return render_template("user_favorites.html")
